065c454041a8fdde2b52c5691017d8f8c699ce4f
Fernando <fernando.augusto.paz@gmail.com>
2021-07-06 22:28:22 +1200

    Revert "Bugfix/18 404 when refreshing app"

--- src/database/firebase/Authentication.js
+++
@@ -1,5 +0,0 @@
-import { auth } from "./FirebaseConfig";
-
-export const signIn = async (email, password) => await auth.signInWithEmailAndPassword(email, password);
-
-export const signOut = async () => await auth.signOut();
